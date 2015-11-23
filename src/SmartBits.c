48e4dc2442c459cc5db1fd258632b9da079afac0
Gustave (Gus) Arthur Monod <gustave.monod@insa-lyon.fr>
2015-11-23 15:00:01 +0100

    Renaming globals

--- src/SmartBits.c
+++ src/SmartBits.c
@@ -1,30 +1,30 @@
 // Standard includes
 #include "pebble.h"
 
-// --------- Defining global constants
+// --------- Global constants
+static const int kSpace = 4;
+static const int kDotRadius = 12;
 
-#define SCREEN_WIDTH  144
-#define SCREEN_HEIGHT 168
+static const int kMonthColumn = 0;
+static const int kDayOfMonthColumn = 1;
+static const int kHourColumn = 2;
+static const int kMinuteColumn = 3;
+static const int kSecondColumn = 4;
 
-#define NB_COLS    5
-#define NB_ROWS    6
-#define SPACE      4
-#define DOT_RADIUS 12
+static const int kWeekDayLine = 5;  // 6th line
+// --------- End of global constants
 
-enum Column { C_MONTH, C_MONTH_DAY, C_HOUR, C_MINUTE, C_SECOND };
-// 6th line
-#define L_WEEK_DAY 5
-
-// --------- End of definition of global constants
-
-Window *window;
+// --------- Globals
+static Window *s_window;
 
 // Layer containing all the circles (LEDs)
-Layer *led_layer;
+static Layer *s_time_layer;
 
 // Layer showing the battery status
-Layer *battery_layer;
+static Layer *s_battery_layer;
+// --------- End of globals
 
+// --------- Function definitions
 // Updates the layer containing all LEDs when the time changes
 static void time_update(Layer *me, GContext *ctx);
 
@@ -39,27 +39,28 @@ static GPoint getCenter(int col, int row);
 
 // Changes the state of a given LED to on (on=true) or off (on=false)
 static void toggle_led(GContext *ctx, int col, int row, bool on);
+// --------- End of function definitions
 
 // Called once per second
 static void handle_second_tick(struct tm* tick_time, TimeUnits units_changed) {
-  layer_mark_dirty(led_layer);
+  layer_mark_dirty(s_time_layer);
 }
 
 // Handle the start-up of the app
 static void init(void) {
   // Create our app's base window
-  window = window_create();
-  window_stack_push(window, true /* Animated */);
-  window_set_background_color(window, GColorBlack);
+  s_window = window_create();
+  window_stack_push(s_window, true /* Animated */);
+  window_set_background_color(s_window, GColorBlack);
 
-  Layer *root_layer = window_get_root_layer(window);
+  Layer *root_layer = window_get_root_layer(s_window);
   GRect root_frame = layer_get_frame(root_layer);
 
-  led_layer = layer_create(root_frame);
-  layer_set_update_proc(led_layer, &time_update);
-  layer_add_child(root_layer, led_layer);
+  s_time_layer = layer_create(root_frame);
+  layer_set_update_proc(s_time_layer, &time_update);
+  layer_add_child(root_layer, s_time_layer);
 
-  battery_layer = layer_create(root_frame);
+  s_battery_layer = layer_create(root_frame);
 
   // Ensures time is displayed immediately (will break if NULL tick event accessed).
   // (This is why it's a good idea to have a separate routine to do the update itself.)
@@ -72,8 +73,8 @@ static void init(void) {
   // Before the battery changes, lets see what its value is:
   battery_update(battery_state_service_peek());
 
-  layer_add_child(root_layer, led_layer);
-  layer_add_child(root_layer, battery_layer);
+  layer_add_child(root_layer, s_time_layer);
+  layer_add_child(root_layer, s_battery_layer);
 }
 
 // Handle the destruction of the app
@@ -81,9 +82,9 @@ static void deinit(void) {
   tick_timer_service_unsubscribe();
   battery_state_service_unsubscribe();
 //  bluetooth_connection_service_unsubscribe();
-  layer_destroy(led_layer);
-  layer_destroy(battery_layer);
-  window_destroy(window);
+  layer_destroy(s_time_layer);
+  layer_destroy(s_battery_layer);
+  window_destroy(s_window);
 }
 
 // The main event/run loop for our app
@@ -106,42 +107,42 @@ static void time_update(Layer *me, GContext *ctx) {
   unsigned short month        = t->tm_mon + 1;
   unsigned short week_day     = 0 != t->tm_wday ? t->tm_wday : 7;// 0 as 7
 
-  toggle_led(ctx, C_SECOND, 0, t->tm_sec & 1);
-  toggle_led(ctx, C_SECOND, 1, t->tm_sec & 2);
-  toggle_led(ctx, C_SECOND, 2, t->tm_sec & 4);
-  toggle_led(ctx, C_SECOND, 3, t->tm_sec & 8);
-  toggle_led(ctx, C_SECOND, 4, t->tm_sec & 16);
-  toggle_led(ctx, C_SECOND, 5, t->tm_sec & 32);
+  toggle_led(ctx, kSecondColumn, 0, t->tm_sec & 1);
+  toggle_led(ctx, kSecondColumn, 1, t->tm_sec & 2);
+  toggle_led(ctx, kSecondColumn, 2, t->tm_sec & 4);
+  toggle_led(ctx, kSecondColumn, 3, t->tm_sec & 8);
+  toggle_led(ctx, kSecondColumn, 4, t->tm_sec & 16);
+  toggle_led(ctx, kSecondColumn, 5, t->tm_sec & 32);
 
-  toggle_led(ctx, C_MINUTE, 0, t->tm_min & 1);
-  toggle_led(ctx, C_MINUTE, 1, t->tm_min & 2);
-  toggle_led(ctx, C_MINUTE, 2, t->tm_min & 4);
-  toggle_led(ctx, C_MINUTE, 3, t->tm_min & 8);
-  toggle_led(ctx, C_MINUTE, 4, t->tm_min & 16);
-  toggle_led(ctx, C_MINUTE, 5, t->tm_min & 32);
+  toggle_led(ctx, kMinuteColumn, 0, t->tm_min & 1);
+  toggle_led(ctx, kMinuteColumn, 1, t->tm_min & 2);
+  toggle_led(ctx, kMinuteColumn, 2, t->tm_min & 4);
+  toggle_led(ctx, kMinuteColumn, 3, t->tm_min & 8);
+  toggle_led(ctx, kMinuteColumn, 4, t->tm_min & 16);
+  toggle_led(ctx, kMinuteColumn, 5, t->tm_min & 32);
 
-  toggle_led(ctx, C_HOUR, 0, hour & 1);
-  toggle_led(ctx, C_HOUR, 1, hour & 2);
-  toggle_led(ctx, C_HOUR, 2, hour & 4);
-  toggle_led(ctx, C_HOUR, 3, hour & 8);
-  toggle_led(ctx, C_HOUR, 4, is_24h_style ? hour & 16 : pm);
+  toggle_led(ctx, kHourColumn, 0, hour & 1);
+  toggle_led(ctx, kHourColumn, 1, hour & 2);
+  toggle_led(ctx, kHourColumn, 2, hour & 4);
+  toggle_led(ctx, kHourColumn, 3, hour & 8);
+  toggle_led(ctx, kHourColumn, 4, is_24h_style ? hour & 16 : pm);
   // last bit of hour is week_day lowest bit
-  toggle_led(ctx, C_HOUR, L_WEEK_DAY, week_day & 1);
+  toggle_led(ctx, kHourColumn, kWeekDayLine, week_day & 1);
 
-  toggle_led(ctx, C_MONTH_DAY, 0, t->tm_mday & 1);
-  toggle_led(ctx, C_MONTH_DAY, 1, t->tm_mday & 2);
-  toggle_led(ctx, C_MONTH_DAY, 2, t->tm_mday & 4);
-  toggle_led(ctx, C_MONTH_DAY, 3, t->tm_mday & 8);
-  toggle_led(ctx, C_MONTH_DAY, 4, t->tm_mday & 16);
+  toggle_led(ctx, kDayOfMonthColumn, 0, t->tm_mday & 1);
+  toggle_led(ctx, kDayOfMonthColumn, 1, t->tm_mday & 2);
+  toggle_led(ctx, kDayOfMonthColumn, 2, t->tm_mday & 4);
+  toggle_led(ctx, kDayOfMonthColumn, 3, t->tm_mday & 8);
+  toggle_led(ctx, kDayOfMonthColumn, 4, t->tm_mday & 16);
   // last bit of hour is week_day middle bit
-  toggle_led(ctx, C_MONTH_DAY, L_WEEK_DAY, week_day & 2);
+  toggle_led(ctx, kDayOfMonthColumn, kWeekDayLine, week_day & 2);
 
-  toggle_led(ctx, C_MONTH, 0, month & 1);
-  toggle_led(ctx, C_MONTH, 1, month & 2);
-  toggle_led(ctx, C_MONTH, 2, month & 4);
-  toggle_led(ctx, C_MONTH, 3, month & 8);
+  toggle_led(ctx, kMonthColumn, 0, month & 1);
+  toggle_led(ctx, kMonthColumn, 1, month & 2);
+  toggle_led(ctx, kMonthColumn, 2, month & 4);
+  toggle_led(ctx, kMonthColumn, 3, month & 8);
   // last bit of month is week_day highest bit
-  toggle_led(ctx, C_MONTH, L_WEEK_DAY, week_day & 4);
+  toggle_led(ctx, kMonthColumn, kWeekDayLine, week_day & 4);
 
 }
 
@@ -160,12 +161,12 @@ static void connection_status_update(bool connected) {
 
 // See top of this file
 static GPoint getCenter(int col, int row) {
-  return GPoint(DOT_RADIUS * (1 + 2 * col) + SPACE * col + SPACE,
-                DOT_RADIUS * (1 + 2 * row) + SPACE * row + SPACE / 2);
+  return GPoint(kDotRadius * (1 + 2 * col) + kSpace * col + kSpace,
+                kDotRadius * (1 + 2 * row) + kSpace * row + kSpace / 2);
 }
 
 // See top of this file
 static void toggle_led(GContext *ctx, int col, int row, bool on) {
   graphics_context_set_fill_color(ctx, on ? GColorWhite : GColorBlack);
-  graphics_fill_circle(ctx, getCenter(col, row), DOT_RADIUS);
+  graphics_fill_circle(ctx, getCenter(col, row), kDotRadius);
 }
